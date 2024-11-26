6847656cf3a2505143bb6e60ea9c56db5761af1c
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-11-27 03:01:57 +0700

    test: fix created potion test

--- server/__test__/createPotion.test.js
+++ server/__test__/createPotion.test.js
@@ -3,13 +3,33 @@ const request = require("supertest");
 const app = require("../app");
 const { generateToken } = require("../helpers/token");
 const { hashPassword } = require("../helpers/hashPassword");
-const { sequelize, User, Genre } = require("../models");
+const { sequelize, User, Genre, Cauldron, Profile } = require("../models");
 
 let users = [
   {
     email: "admin@gmail.com",
     password: hashPassword("123456"),
     isVerified: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+let cauldrons = [
+  {
+    name: "Dragon Cauldron",
+    UserId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+let profiles = [
+  {
+    fullName: "admin",
+    profilePicture:
+      "https://images.pexels.com/photos/6129297/pexels-photo-6129297.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    UserId: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
   },
@@ -33,8 +53,11 @@ beforeAll(async () => {
   try {
     await sequelize.queryInterface.bulkInsert("Users", users);
     await sequelize.queryInterface.bulkInsert("Genres", genres);
+    await sequelize.queryInterface.bulkInsert("Profiles", profiles);
+    await sequelize.queryInterface.bulkInsert("Cauldrons", cauldrons);
     const user = await User.findOne({ where: { email: "admin@gmail.com" } });
-    token = generateToken({ id: user.id });
+    token = generateToken(user.id);
+    // token = access_token;
   } catch (error) {
     console.log("🚀 ~ beforeAll ~ error:", error);
   }
@@ -52,12 +75,23 @@ afterAll(async () => {
       restartIdentity: true,
       cascade: true,
     });
+    await Profile.destroy({
+      truncate: true,
+      restartIdentity: true,
+      cascade: true,
+    });
+    await Cauldron.destroy({
+      truncate: true,
+      restartIdentity: true,
+      cascade: true,
+    });
   } catch (error) {
     console.log("🚀 ~ afterAll ~ error:", error);
   }
 });
 
 describe("create Potion", () => {
+  //   console.log(token, "TOKEN===========");
   let potion = {
     recommendation:
       "In a world ravaged by celestial wars, a disgraced Rune Knight, Kael, haunted by the death of his family, discovers a hidden prophecy foretelling the birth of a child who will either save or destroy their fractured world.  This child is the offspring of Kael and a vengeful, ancient spirit, bound to him through a mystical pact.  Forced to raise this powerful, volatile infant while battling the remnants of the celestial armies and a conspiracy that seeks to exploit the child's immense power, Kael must confront his past traumas and forge unlikely alliances to ensure her survival and the fate of their world.  Guided by a mysterious, sentient cheat-system imbued with forgotten magic, he juggles fatherhood, epic battles, and unraveling the truth of the celestial wars, all while struggling to protect his daughter from those who crave her power.  Their journey is a tumultuous blend of action, heart-wrenching drama, and unexpected tenderness, as Kael rediscovers hope and learns the true meaning of family.",
@@ -82,5 +116,15 @@ describe("create Potion", () => {
 
     expect(response.status).toBe(404);
     expect(response.body.message).toBe("Cauldron not found");
+  });
+
+  test("error unauthorized", async () => {
+    const response = await request(app)
+      .post("/cauldrons/1000/potions")
+      .set("Authorization", `Bearer ${token}1`)
+      .send(potion);
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe("Invalid Token");
   });
 });
