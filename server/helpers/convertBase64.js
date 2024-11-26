b7f78157bc1347fc38d3f07efd2653be306dfc57
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-11-27 04:03:11 +0700

    feat: delete upload helpers and env variables

--- server/helpers/convertBase64.js
+++
@@ -1,8 +0,0 @@
-const convertImgToString = async (file) => {
-  const mimeType = file.mimetype;
-  const stringBase64 = file.buffer.toString("base64");
-  const data = `data:${mimeType};base64,${stringBase64}`;
-  return data;
-};
-
-module.exports = convertImgToString;
