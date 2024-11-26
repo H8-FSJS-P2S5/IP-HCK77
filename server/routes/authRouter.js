3806b593614fafb9ed868c31d861f34776610d93
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-11-27 04:20:21 +0700

    feat: add cauldron guard to check if user is authorized

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,7 +1,6 @@
 const PotionController = require("../controllers/PotionController");
 
 const router = require("express").Router();
-// router.get("/cauldrons/:cauldronId/potions", (req, res)=>{res.json("sss")});
 router.post("/cauldrons/:cauldronId/potions", PotionController.createPotion);
 
 module.exports = router;
