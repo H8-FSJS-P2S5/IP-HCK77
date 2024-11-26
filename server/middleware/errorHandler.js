0691ef1ea986440ae2b17d56a68909e17e5d7c44
FauzhanWahyudi <fauzhanwahyudi@gmail.com>
2024-11-26 16:38:43 +0700

    fix: add email unique contraint error message to errorHandler

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -7,11 +7,10 @@ function errorHandler(err, req, res, next) {
     return;
   } else if (err.name == "Validation Error") {
     res.status(400).json({ message: err.message });
-  } else if (
-    err.name == "SequelizeValidationError" ||
-    err.name == "SequelizeUniqueConstraintError"
-  ) {
+  } else if (err.name == "SequelizeValidationError") {
     res.status(400).json({ message: err.errors[0].message });
+  } else if (err.name == "SequelizeUniqueConstraintError") {
+    res.status(400).json({ message: "Email must be unique" });
   } else if (err.name == "Authentication Failed") {
     res.status(401).json({ message: err.message });
   } else if (err.name == "Forbidden") {
