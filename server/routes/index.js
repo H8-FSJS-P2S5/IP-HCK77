280605d7763ca518c09f2fecc4e1240c48c009ee
FauzhanWahyudi <fauzhanwahyudi@gmail.com>
2024-11-26 18:42:17 +0700

    feat: add genres endpoint withoit recommendation with AI

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,4 @@
+const GenreController = require("../controllers/GenreController");
 const UserController = require("../controllers/UserController");
 const authentication = require("../middleware/authentication");
 const errorHandler = require("../middleware/errorHandler");
@@ -5,6 +6,7 @@ const router = require("express").Router();
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/login/google", UserController.loginGoogle);
+router.get("/genres", GenreController.showGenres);
 // router.use("/", authentication, devRoute);
 router.use(errorHandler);
 
