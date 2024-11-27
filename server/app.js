10dfe7c4e470e9aeb9df52d74dba8ff85b1d2a6f
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-11-28 04:54:01 +0700

    fix: cors forgot to be invoked

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./routes/index");
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); //error because of not invoked
 
