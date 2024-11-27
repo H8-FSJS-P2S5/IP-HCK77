9fd31a1f632fd693da400a4af74d8c1c2fcdabc1
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-11-27 10:42:08 +0700

    feat: change authentication middleware invoke place

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,14 @@
 const GenreController = require("../controllers/GenreController");
 const UserController = require("../controllers/UserController");
-const authentication = require("../middleware/authentication");
 const errorHandler = require("../middleware/errorHandler");
 const router = require("express").Router();
 const authRouter = require("./authRouter");
-
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/login/google", UserController.loginGoogle);
 router.get("/genres", GenreController.showGenres);
-router.use("/", authentication, authRouter);
+router.use("/", authRouter);
 router.use(errorHandler);
 
 module.exports = router;
