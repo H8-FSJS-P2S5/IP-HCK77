ceb1cbeea4758f81127b4cfd9f319ccfd6677db6
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-11-27 11:24:47 +0700

    fix: try 3 - just use index as all router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,39 @@
 const GenreController = require("../controllers/GenreController");
 const UserController = require("../controllers/UserController");
+const CauldronController = require("../controllers/CauldronController");
+const PotionController = require("../controllers/PotionController");
+const ProfileController = require("../controllers/ProfileController");
+const authentication = require("../middleware/authentication");
 const errorHandler = require("../middleware/errorHandler");
+const { guardCauldron } = require("../middleware/authorization");
 const router = require("express").Router();
-const authRouter = require("./authRouter");
+// const authRouter = require("./authRouter");
 
+router.get("/", (req, res) => res.json("HOME"));
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/login/google", UserController.loginGoogle);
 router.get("/genres", GenreController.showGenres);
-router.use("/user", authRouter);
+// router.use("/user", authRouter);
+router.use(authentication);
+router.post("/cauldrons/:cauldronId/potions", PotionController.createPotion);
+router.put("/profile/", ProfileController.updateProfile);
+router.get("/cauldrons", CauldronController.showCauldrons);
+router.put(
+  "/cauldrons/:cauldronId",
+  guardCauldron,
+  CauldronController.updateCauldron
+);
+router.put(
+  "/cauldrons/:cauldronId/potions/:potionId",
+  guardCauldron,
+  PotionController.updatePotion
+);
+router.delete(
+  "/cauldrons/:cauldronId/potions/:potionId",
+  guardCauldron,
+  PotionController.deletePotion
+);
 router.use(errorHandler);
 
 module.exports = router;
